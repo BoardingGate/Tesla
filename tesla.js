130f97d7f7101a2ba1e22e29448eb72b5a5edefc
BoardingGate <simbioticsystem@gmail.com>
2025-08-12 00:37:20 +0200

    Update tesla.js

--- tesla.js
+++ tesla.js
@@ -1127,6 +1127,8 @@ class CombinedMapControl {
     let progressBarForceShowUntil = 0;
     let lastProcessedStepForLock = null;
     const ARRIVAL_MODAL_THRESHOLD_METERS = 20
+    LET isInitialMapLoadAnimationActive= true;
+    
     
     const PERFORMANCE_RATIO_ROUTE_KEY = 'perf_ratio_route';
     const PERFORMANCE_RATIO_ALERTS_KEY = 'perf_ratio_alerts';
