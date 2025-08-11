8b7e0e19adb8d536904c724e51264497a35a34d1
BoardingGate <simbioticsystem@gmail.com>
2025-08-11 23:19:13 +0200

    Update tesla.js

--- tesla.js
+++ tesla.js
@@ -1,6 +1,3 @@
-<head>
-  V13
-</head>
 
 //ofu mapbox
                                                                                                     mapboxgl.accessToken = "pk.eyJ1IjoiYm9hcmRpbmdnYXRlMTEiLCJhIjoiY21kOHdtMGU4MDEzaTJpcGh4cng0c3hmMiJ9.hDdGn_5VOMkN_bTuUowtRw";
